7ca3388f6b72a56b378a225b8f18b20e121f45db
Jason <jasonabbatheleast@gmail.com>
2024-05-30 18:12:44 +0100

    feat: initialised project structure

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+         I LOVE YOU 🧡🧡🧡🧡THERESA SALIU 😘😘😘
+         Will you spend forever with me?
+
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
